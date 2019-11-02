dd205ab6ede94482bbe83507713d0b4cb479aba0
Katarzyna Maryańska <kmaryanska@gmail.com>
2019-11-02 23:03:43 +0100

    add navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-import { Root, Wrapper, Logo } from "./styles";
+import { Root, Wrapper, Logo, Navbar, NavLink } from "./styles";
 
 export const Header = () => (
   <Root>
     <Wrapper>
       <Logo />
+      <Navbar>
+        <li>
+          <NavLink href={"#"}>Start</NavLink>
+        </li>
+        <li>
+          <NavLink href={"#"}>My Profile</NavLink>
+        </li>
+      </Navbar>
     </Wrapper>
   </Root>
 );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,6 +5,22 @@ import logoImg from "./assets/logo.svg";
 
 export const Logo = () => <img alt="Startup Development House" src={logoImg} />;
 
+export const Navbar = styled.ul`
+  width: 10rem;
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NavLink = styled.a`
+  font-size: ${({ theme }) => theme.font.size.md};
+  color: ${({theme}) => theme.color.black};
+  font-weight: ${({ theme }) => theme.font.weight.normal};
+  text-decoration: none;
+`;
+
 export const Root = styled.header`
   height: 5rem;
 `;
